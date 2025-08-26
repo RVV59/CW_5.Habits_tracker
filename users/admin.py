69158bc3d5b8d6f905c0458c2f18896c8e40c210
RVV59 <reshetov664@gmail.com>
2025-08-26 19:44:11 -0400

    faker, permissions, fake_data. superuser are added, Tuesday, August 26, 2025 7:44:00 PM

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    """
+    Кастомная конфигурация для отображения модели User в админ-панели.
+    """
+    list_display = ('email', 'phone', 'city', 'is_staff', 'is_active')
+    search_fields = ('email', 'phone')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('phone', 'city', 'avatar')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password', 'password2'),
+        }),
+    )
+    ordering = ('email',)
